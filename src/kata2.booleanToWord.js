fcd54a5cf667191d9a44c4198e5939ec87f54b63
Sam Tovey <samtovey13@gmail.com>
2020-07-05 22:37:03 +0100

    completed kata2 booleanToWord

--- src/kata2.booleanToWord.js
+++ src/kata2.booleanToWord.js
@@ -1,3 +1,11 @@
-const booleanToWord = boolean => {};
+const booleanToWord = boolean => {
+  if (typeof boolean !== "boolean") {
+        return "not a boolean data type";
+    }
+    if (boolean === true) {
+        return "yes";
+    }
+    return "no";
+};
 
 module.exports = booleanToWord;
--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,17 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it('returns a string', () => {
+    const result = booleanToWord();
+    expect(typeof result).toBe('string');
+  });
+  it('returns the correct string', () => {
+    expect(booleanToWord(true)).toBe("yes");
+    expect(booleanToWord(false)).toBe("no");
+  });
+  it('throws an error if argument is not valid', () => {
+    expect(booleanToWord(5)).toBe("not a boolean data type");
+    expect(booleanToWord([true, false])).toBe("not a boolean data type");
+    expect(booleanToWord("true")).toBe("not a boolean data type");
+  });
 });
