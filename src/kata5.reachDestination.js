d023c5677af13ee2d0999a2b58679bba32c3c0af
Sam Tovey <samtovey13@gmail.com>
2020-07-05 23:56:18 +0100

    completed kata5 reachDestination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    let time = (distance / speed);
+    time = Math.round(time*2)/2;
+    return "I should be there in " + time + " hours.";
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,9 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toBe("I should be there in 4.5 hours.");
+    expect(reachDestination(30, 67)).toBe("I should be there in 0.5 hours.");
+    expect(reachDestination(30, 40)).toBe("I should be there in 1 hours.");
+  });
 });
