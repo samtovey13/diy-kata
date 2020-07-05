0b14eae5869fbebbca2ff919ae2d553d346287c5
Sam Tovey <samtovey13@gmail.com>
2020-07-05 23:28:37 +0100

    completed kata4 humanCatDogYears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,22 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+    if ((typeof number) != "number") {
+        return "Error: invalid input";
+    }
+    let catYears;
+    let dogYears;
+    if (number >= 1) {
+        catYears = 15;
+        dogYears = 15;
+    }
+    if (number >= 2) {
+        catYears += 9;
+        dogYears += 9;
+    }
+    if (number >= 3) {
+        catYears += (number - 2) * 4;
+        dogYears += (number - 2) * 5;
+    }
+    return [number, catYears, dogYears];
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,19 @@
 const { humanCatDogYears } = require("../src");
+const { TestScheduler } = require("jest");
 
-// Look Ma, no handlebars!!!
+describe("converts human years to cat and dog years", () => {
+    test("returns an array of 3 items", () => {
+        const input = 10;
+        const result = humanCatDogYears(input);
+        expect(typeof result).toBe("object");
+        expect(result.length).toBe(3);
+    });
+    test("returns an array of 3 numbers [humanYears, catYears, dogYears]", () => {
+        expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+        expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+        expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+    });
+    test("throws an error if input is not a number", () => {
+        expect(humanCatDogYears("str")).toBe("Error: invalid input");
+    });
+});
