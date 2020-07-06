0e09a81425a657f1f827cd8eb215af454acde558
Sam Tovey <samtovey13@gmail.com>
2020-07-06 13:44:21 +0100

    completed kata6 joinNames

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,13 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+  const namesArr = namesObj.map(item => item.name);
+  if (namesArr.length === 1) {
+    return namesArr.toString();
+  }
+  const last2names = namesArr.splice(-2).join(" \& ");
+  if (namesArr.length === 0) {      // length=0 because namesArr has been spliced
+    return last2names;
+  }
+  return namesArr.join(", ") + ", " + last2names;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,21 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames([{
+      name: 'Bart'
+    }, {
+      name: 'Lisa'
+    }, {
+      name: 'Maggie'
+    }])).toBe('Bart, Lisa & Maggie');
+    expect(joinNames([{name: 'Aang'}, {name: 'Momo'}, {name: 'Appa'}, {name: 'Sokka'}, {name: 'Katara'}, {name: 'Toph'}]))
+    .toBe('Aang, Momo, Appa, Sokka, Katara & Toph');
+  });
+  test("returns string of single name when given only 1 object", () => {
+    expect(joinNames([{name: 'Madonna'}])).toBe('Madonna');
+  });
+  test("works for 2 objects", () => {
+    expect(joinNames([{name: 'Bonnie'}, {name: 'Clyde'}])).toBe('Bonnie & Clyde');
+  });
 });
