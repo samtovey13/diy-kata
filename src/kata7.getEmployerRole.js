d1774c49f10928fcec72b5470cbf0aed440454fc
Sam Tovey <samtovey13@gmail.com>
2020-07-06 14:03:54 +0100

    completed kata7 getEmployerRole

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,11 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+    if (!employees.find(object => object.name === employeeName)) {
+        return "Error: employee name not recognised";
+    }
+    if (!employees.find(object => object.name === employeeName).role) {
+        return "Error: no role found for this employee";
+    }
+    return employees.find(object => object.name === employeeName).role;
+};
 
 module.exports = getEmployerRole;
--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,37 @@
 const { getEmployerRole } = require("../src");
 
+const employees = [{
+
+  name: 'Satti',
+
+  role: 'Developer'
+
+}, {
+
+  name: 'Jenny',
+
+  role: 'Sales Associate'
+
+}, {
+
+  name: 'Javid',
+
+  role: 'Human Recommended Reading Assistant'
+}, {
+
+  name: 'Matthew',
+
+}];
+
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  test("returns the employee's role in the company", () => {
+    expect(getEmployerRole('Javid', employees)).toBe('Human Recommended Reading Assistant');
+    expect(getEmployerRole('Jenny', employees)).toBe('Sales Associate');
+  });
+  test("throws an error if no matching name found", () => {
+    expect(getEmployerRole('Spongebob', employees)).toBe('Error: employee name not recognised');
+  });
+  test("throws an error if no corresponding 'role' property found", () => {
+    expect(getEmployerRole('Matthew', employees)).toBe('Error: no role found for this employee');
+  });
 });
